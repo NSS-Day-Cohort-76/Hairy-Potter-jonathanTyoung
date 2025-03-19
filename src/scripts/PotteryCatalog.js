5d44502a6036205c66f71ee86b3a94ca86ba5e64
Jon Young <youngt.jonathan>
2025-03-19 10:18:30 -0500

    updated modules

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,13 @@
+const catalog = []
+export const toSellOrNotToSell = (pottery, weight) => {
+    if (weight >= 6) {
+        pottery.price = 40;
+} else (weight < 6) {
+        pottery.price = 20;
+} 
+
+
+
+// export const getFish = () => {
+//     return structuredClone(database.fish);
+// }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,33 @@
-// Imports go first
+import { makePottery } from './PotteryWheel.js';
+import { firePottery } from './Kiln.js';
 
 
 
-// Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+// Make the pieces in the PotteryWheel
+const bowl = makePottery ("bowl", 2, 3);
+console.log(bowl);
 
+const cup = makePottery ("cup", 4, 5);
+console.log(cup);
 
-// Fire each piece of pottery in the kiln
+const house = makePottery ("house", 2, 5);
+console.log(house);
 
+const fork= makePottery ("fork", .5, 6);
+console.log(fork);
 
-// Determine which ones should be sold, and their price
+const vase = makePottery ("vase", 10, 12);
+console.log(vase);
 
+// Fire each piece of the pottery
 
-// Invoke the component function that renders the HTML list
-
+const firedBowl = firePottery(bowl, 2199)
+console.log(firedBowl)
+const firedCup = firePottery(cup, 2300)
+console.log(firedCup)
+const firedHouse = firePottery(house, 2500)
+console.log(firedHouse)
+const firedFork = firePottery(fork, 1200)
+console.log(firedFork)
+const firedVase = firePottery(vase, 3000)
+console.log(firedVase)
